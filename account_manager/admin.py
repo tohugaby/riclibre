93c45aa06246e681e65575bbcc99efe0f0f7a839
tom.gabriele <tom.gabriele@free.fr>
2019-03-05 16:37:24 +0100

    feat(account_manager): create custom account model and admin
    - reference custom account model as auth model in settings
    
    5c7e85a0504c097dd204d7c0 create custom account model and admin
    Closes https://trello.com/c/Sr7XxYej/1-create-custom-account-model-and-admin

--- account_manager/admin.py
+++ account_manager/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from account_manager.models import CustomUser
+
+
 # Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    """
+    admin class for CustomUser model.
+    """
+    pass
--- account_manager/models.py
+++ account_manager/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    """
+    A custom user model
+    """
+    email = models.EmailField(_('email address'), unique=True)
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
